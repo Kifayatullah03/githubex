8ee09438c61710d2b786ca5c0cd04c5c39c26b84
kifayatullah03 <kifayat.amini001@gmail.com>
2022-06-01 06:31:13 +0500

    1stcommit

---
+++ src/githubex/GithubEx3.java
@@ -0,0 +1,13 @@
+package githubex;
+
+public class GithubEx3 {
+
+	public static void main(String[] args) {
+
+		System.out.print("today we learn github");
+		System.out.print("today is a very good dAY");
+		
+		
+	}
+
+}
